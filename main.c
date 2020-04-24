aa5e65c4ff1ac7ab6d2249f7ba6cd0b15078b310
Holden Gjuka <holdengjuka@gmail.com>
2020-04-23 18:06:33 -0700

    Seperated logic for reading and displaying x,y,z vals, now need to implement mutex locks.

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 // Class: Operating Systems
 // Instructor: Dr. Caley
 // Date: 4/21/2020
-// This program takes in data from an accelerometer and prints the data to console.
-// Utilizes threading/multiple processes. Runs on Raspberry Pi 4 Model B Rev 1.1,
-// running the Raspbian OS.
+// This program takes in data from an accelerometer and prints the data 
+// to console. Utilizes threading. Runs on Raspberry Pi 4 Model B Rev 
+// 1.1, running the Raspbian OS.
 // TO COMPILE:
 // gcc -pthread -o a main.c -lwiringPi
 // ./a
@@ -20,99 +20,126 @@
 #include <wiringPiI2C.h>
 
 
-const int limit = 1000;
-int counter = 0;
-int nums[1000];
 pthread_mutex_t mutex1;
 
-
-void generate(){
-    pthread_mutex_lock(&mutex1);
-    counter++;
-    srand(time(0));
-    nums[counter] = rand() % 100 + 1;
-    pthread_mutex_unlock(&mutex1);
+//reads current x value from accelerometer
+int generate_x(int fd){
+    int x_lsb = wiringPiI2CReadReg8(fd, 1);  //least_significant_byte LSB
+    int x_msb = wiringPiI2CReadReg8(fd, 2)>>4;  //most_significant_byte MSB
+    int x_sign = x_msb>>7;
+    int x_final = ((x_msb << 4| x_lsb) ^ (x_sign<<7)) | (x_sign<<31);
+    if(x_sign == 1) { x_final = (x_sign<<31) | ~x_final; }
+    return x_final;
 }
 
-
-void* thread_1_main(){
-    unsigned int exec_period = 1000000;
-    while(1){
-        usleep(exec_period);
-        generate();
-    }
+//reads current y value from accelerometer
+int generate_y(int fd){
+    int y_lsb = wiringPiI2CReadReg8(fd, 3);
+    int y_msb = wiringPiI2CReadReg8(fd, 4)>>4;
+    int y_sign = y_msb>>7;
+    int y_final = ((y_msb << 4| y_lsb) ^ (y_sign<<7)) | (y_sign<<31);
+    if(y_sign == 1) { y_final = (y_sign<<31) | ~y_final; }
+    return y_final;
 }
 
-double average() {
+//reads current z value from accelerometer
+int generate_z(int fd){
+    int z_lsb = wiringPiI2CReadReg8(fd, 5);
+    int z_msb = wiringPiI2CReadReg8(fd, 6)>>4;
+    int z_sign = z_msb>>7;
+    int z_final = ((z_msb << 4| z_lsb) ^ (z_sign<<7)) | (z_sign<<31);
+    if(z_sign == 1) { z_final = (z_sign<<31) | ~z_final; }
+    return z_final;
+}
+
+//calculates the average value of an array
+double average(int counter, int *vals) {
     double sum = 0.0;
     for(int i = 0; i < counter; i++){
-        sum += nums[i];
+        sum += vals[i];
     }
     return sum / counter;
 }
 
-int find_min() {
-    int min = 101;
+//finds the minimum value of an array
+int find_min(int counter, int *vals) {
+    int min = 1000000;
     for(int i = 0; i < counter; i++){
-        if(nums[counter] < min){
-            min = nums[counter];
+        if(i == 0 || vals[i] < min){
+            min = vals[i];
         }
     }
     return min;
 }
 
-int find_max() {
-    int max;
+//finds the maximum value of an array
+int find_max(int counter, int *vals) {
+    int max = 0;
     for(int i = 0; i < counter; i++){
-        if(i == 0 || nums[i] > max){
-            max = nums[i];
+        if(i == 0 || vals[i] > max){
+            max = vals[i];
         }
     }
     return max;
 }
 
-void* thread_2_main(){
-    unsigned int exec_period = 1000000;
+//displays the avg, min, and max of an array
+void displayStats(int counter, int *vals){
+    printf("Average: %d\n", average(counter, vals));
+    printf("Minimum: %d\n", find_min(counter, vals));
+    printf("Max: %d\n", find_max(counter, vals));
+}
+
+//handles the x value
+void* runner1(int *fd_ptr){
+    int fd = *fd_ptr;
+    int x_counter = 0;
+    int x_vals[1000];
     while(1){
-        usleep(exec_period);
-        printf("Average: %f\n", average());
-        printf("Minimum: %i\n", find_min());
-        printf("Max: %i\n", find_max());
+        int x = generate_x(fd);
+        x_counter++;
+        displayStats(x_counter, x_vals);
+    }
+}
+
+//handles the y value
+void* runner2(int *fd_ptr){
+    int fd = *fd_ptr;
+    int y_counter = 0;
+    int y_vals[1000];
+    while(1){
+        int y = generate_y(fd);
+        y_counter++;
+        displayStats(y_counter, y_vals);
+    }
+}
+
+//handles the z value
+void* runner3(int *fd_ptr){
+    int fd = *fd_ptr;
+    int z_counter = 0;
+    int z_vals[1000];
+    while(1){
+        int z = generate_z(fd);
+        z_counter++;
+        displayStats(z_counter, z_vals);
     }
 }
 
 int main(){
-    int fd = wiringPiI2CSetup(0x1d);
-    int val = wiringPiI2CRead(fd);
-    //Dr. Caley, you said in lecture that int produces weird behavior but
-    //that makes no sense to me because I can't see how it would do that?
-    int x_lsb = wiringPiI2CReadReg8(fd, 1);  //least_significant_byte LSB
-    int x_msb = wiringPiI2CReadReg8(fd, 2)>>4;  //most_significant_byte MSB
-    int x_sign = x_msb>>7;
-    int y_lsb = wiringPiI2CReadReg8(fd, 3);
-    int y_msb = wiringPiI2CReadReg8(fd, 4)>>4;
-    int y_sign = y_msb>>7;
-    int z_lsb = wiringPiI2CReadReg8(fd, 5);
-    int z_msb = wiringPiI2CReadReg8(fd, 6)>>4;
-    int z_sign = z_msb>>7;
-    int x_final = ((x_msb << 4| x_lsb) ^ (x_sign<<7)) | (x_sign<<31);
-    int y_final = ((y_msb << 4| y_lsb) ^ (y_sign<<7)) | (y_sign<<31);
-    int z_final = ((z_msb << 4| z_lsb) ^ (z_sign<<7)) | (z_sign<<31);
-    if(x_sign == 1) { x_final = (x_sign<<31) | ~x_final; }
-    if(y_sign == 1) { y_final = (y_sign<<31) | ~y_final; }
-    if(z_sign == 1) { z_final = (z_sign<<31) | ~z_final; }
-    printf("x: %d, y: %d, z: %d", x_final, y_final, z_final);
     
-
+    int fd = wiringPiI2CSetup(0x1d);
+    int *fd_ptr = &fd;
     pthread_t tid1;
     pthread_t tid2;
+    pthread_t tid3;
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     
-    int* nums_ptr = nums;
-    pthread_create(&tid1, &attr, (void*) &thread_1_main, nums_ptr);
-    pthread_create(&tid2, &attr, (void*) &thread_2_main, nums_ptr);
+    pthread_create(&tid1, &attr, (void*) &runner1, fd_ptr);
+    pthread_create(&tid2, &attr, (void*) &runner2, fd_ptr);
+    pthread_create(&tid3, &attr, (void*) &runner3, fd_ptr);
 
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
