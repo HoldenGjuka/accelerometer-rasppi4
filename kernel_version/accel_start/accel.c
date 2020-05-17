cee1b13dc1792f4b1b57176916a2802b176eb81a
Holden Gjuka <holdengjuka@gmail.com>
2020-05-16 19:38:58 -0700

    Removed user-space commands and using kobject_create_and_add now

--- kernel_version/accel_start/accel.c
+++ kernel_version/accel_start/accel.c
@@ -1,7 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
-
 #include <linux/i2c.h>
+#include <linux/kobject.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Quiic Accelerometer Sysfs Module");
@@ -10,6 +10,10 @@ MODULE_VERSION("0.1");
 struct i2c_adapter* i2c_dev;
 struct i2c_client* i2c_client;
 
+//NOTES:
+//will need a separate file for each value, with only one value.
+//Thus the module is dumb, does not perform analysis of any sort.
+
 static struct i2c_board_info __initdata board_info[] =  {
 	{
 		I2C_BOARD_INFO("MMA8452Q", 0x1d),
@@ -17,8 +21,10 @@ static struct i2c_board_info __initdata board_info[] =  {
 };
 
 static int __init accel_init(void) {
+	//Declare variables
 	s32 whoAmIResult;
 	s32 xAxisValue;
+	int fd;
 
 	printk(KERN_DEBUG "accelerometer init\n");
 
@@ -36,23 +42,21 @@ static int __init accel_init(void) {
 	i2c_smbus_write_byte_data(i2c_client, 0x2A, 0x01);
 	whoAmIResult = i2c_smbus_read_byte_data(i2c_client, 0x0D);
 	printk(KERN_DEBUG "who am I result: 0x%x\n", whoAmIResult);
-	int fd;
-
-	int check;
-    char* dirname = "test_dir_TEST";
-
-    check = mkdir(dirname);
-
-    // check if directory is created or not
-    if (!check)
-        printf("Directory created\n");
-    else {
-        printf("Unable to create directory\n");
-        exit(1);
-    }
-	//fd = open("sys/fs/test_dir_TEST", O_WRONLY);
 	xAxisValue = i2c_smbus_read_byte_data(i2c_client, 0x01);
 	printk(KERN_DEBUG "x-axis value: 0x%x\n", xAxisValue);
+	
+    char* dirname = "test_dir_TEST";
+
+    struct kobject *example_kobject;
+	example_kobject = kobject_create_and_add(dirname)
+
+    if (!check) // check if directory is created or not
+        printk(KERN_INFO "Directory created\n");
+    else {
+        printk(KERN_INFO "Unable to create directory\n");
+        goto exit;
+    }
+	//fd = open("sys/fs/test_dir_TEST", O_WRONLY);
 
 exit:
 	return 0;
